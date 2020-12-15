2d931cc5328c71c75f63ec9efc141547a933fa49
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-12-15 10:48:04 +0000

    Yet more tidying.

--- smg/relocalisation/aruco_pnp_relocaliser.py
+++ smg/relocalisation/aruco_pnp_relocaliser.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 from smg.relocalisation.backends.sparse_pnp_backend import SparsePnPBackend
 from smg.relocalisation.frontends.aruco_correspondence_generator import ArUcoCorrespondenceGenerator
@@ -15,35 +15,33 @@ class ArUcoPnPRelocaliser:
         """
         Construct an ArUco-PnP relocaliser.
 
-        :param fiducials:   The fiducials.
+        :param fiducials:   The fiducials (a map from fiducial names to their known 3D positions).
         """
         self.__correspondence_generator: ArUcoCorrespondenceGenerator = ArUcoCorrespondenceGenerator(fiducials)
 
     # PUBLIC METHODS
 
-    def estimate_pose(self, colour_image: np.ndarray, intrinsics: Tuple[float, float, float, float], *,
+    def estimate_pose(self, image: np.ndarray, intrinsics: Tuple[float, float, float, float], *,
                       draw_detections: bool = False, print_correspondences: bool = False) -> np.ndarray:
         """
-        Try to estimate the pose of the camera from a colour image of the scene being viewed.
+        Try to estimate the pose of the camera from an image of the scene being viewed.
 
-        :param colour_image:            The colour image of the scene being viewed.
+        :param image:                   The image of the scene being viewed.
         :param intrinsics:              The intrinsics for the colour camera.
         :param draw_detections:         Whether or not to draw the ArUco markers after detecting them (for debugging).
         :param print_correspondences:   Whether or not to print the generated correspondences (for debugging).
         :return:                        The estimated camera-to-world transform, if possible, or None otherwise.
         """
-        # TODO
+        # Generate 2D-to-3D correspondences that can be used to estimate the pose.
+        correspondences: List[np.ndarray] = self.__correspondence_generator.generate_correspondences(
+            image, draw_detections=draw_detections, print_correspondences=print_correspondences
+        )
+
+        # Estimate the pose using the generated correspondences.
         fx, fy, cx, cy = intrinsics
         camera_matrix: np.ndarray = np.array([
             [fx, 0, cx],
             [0, fy, cy],
             [0, 0, 1]
         ])
-
-        # TODO
-        correspondences = self.__correspondence_generator.generate_correspondences(
-            colour_image, draw_detections=draw_detections, print_correspondences=print_correspondences
-        )
-
-        # TODO
         return SparsePnPBackend.estimate_pose(correspondences, camera_matrix)
--- smg/relocalisation/frontends/aruco_correspondence_generator.py
+++ smg/relocalisation/frontends/aruco_correspondence_generator.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
 class ArUcoCorrespondenceGenerator:
@@ -13,34 +13,34 @@ class ArUcoCorrespondenceGenerator:
         """
         Construct an ArUco correspondence generator.
 
-        :param fiducials:   The fiducials.
+        :param fiducials:   The fiducials (a map from fiducial names to their known 3D positions).
         """
         self.__fiducials: Dict[str, np.ndarray] = fiducials
         self.__marker_dict: cv2.aruco_Dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
 
     # PUBLIC METHODS
 
-    def generate_correspondences(self, img: np.ndarray, *, draw_detections: bool = False,
+    def generate_correspondences(self, image: np.ndarray, *, draw_detections: bool = False,
                                  print_correspondences: bool = False) -> List[np.ndarray]:
         """
         Generate 2D-to-3D correspondences via detecting ArUco markers in the specified image.
 
-        :param img:                     The image in which to detect the markers.
+        :param image:                   The image in which to detect the markers.
         :param draw_detections:         Whether or not to draw the markers after detecting them (for debugging).
         :param print_correspondences:   Whether or not to print the generated correspondences (for debugging).
-        :return:                        The generated correspondences.
+        :return:                        The generated correspondences, as a list of [x, y, X, Y, Z] arrays.
         """
-        correspondences: [np.ndarray] = []
+        correspondences: List[np.ndarray] = []
 
         # Detect the markers in the image.
         parameters = cv2.aruco.DetectorParameters_create()
-        corners, marker_ids, _ = cv2.aruco.detectMarkers(img, self.__marker_dict, parameters=parameters)
+        corners, marker_ids, _ = cv2.aruco.detectMarkers(image, self.__marker_dict, parameters=parameters)
 
         # Draw the markers if requested.
         if draw_detections:
-            img_markers = cv2.aruco.drawDetectedMarkers(img.copy(), corners, marker_ids) \
-                if marker_ids is not None else img.copy()
-            cv2.imshow("Markers", img_markers)
+            marker_image: np.ndarray = cv2.aruco.drawDetectedMarkers(image.copy(), corners, marker_ids) \
+                if marker_ids is not None else image.copy()
+            cv2.imshow("Markers", marker_image)
             cv2.waitKey(1)
 
         # If no markers were detected, early out.
@@ -51,15 +51,15 @@ class ArUcoCorrespondenceGenerator:
 
         # For each marker:
         for i in range(len(marker_ids)):
-            marker_id = str(marker_ids[i][0])
+            marker_id: str = str(marker_ids[i][0])
 
             # For each corner:
             for j in range(4):
                 # Determine the name that the corresponding fiducial would have.
-                fiducial_id = marker_id + "_" + str(j)
+                fiducial_id: str = f"{marker_id}_{j}"
 
                 # Look to see if we know its position in 3D space.
-                pos3d = self.__fiducials.get(fiducial_id, None)
+                pos3d: Optional[np.ndarray] = self.__fiducials.get(fiducial_id)
 
                 # If we don't, ignore this corner and carry on.
                 if pos3d is None:
@@ -67,7 +67,6 @@ class ArUcoCorrespondenceGenerator:
 
                 # If we do, add a 2D-to-3D correspondence for the corner.
                 pos2d = corners[i][0][j]
-                # correspondences.append((Vec2(pos2d[0], pos2d[1]), pos3d))
                 correspondences.append(np.array([*pos2d, *pos3d]))
 
                 # Print the correspondence if requested.

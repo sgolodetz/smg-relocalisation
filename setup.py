ae7cc6f0cc5df066acd4fd6f7ef783c2c06a4c3c
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-12-16 17:23:15 +0000

    Update setup.py.

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
     include_package_data=True,
     install_requires=[
         "numpy",
-        "opencv-contrib-python==3.4.2.16"
+        "opencv-contrib-python==3.4.2.16",
+        "vg"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",

abe66f546d45a528d2cd0a85e154e88cd6df8dff
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-12-18 15:56:25 +0000

    Yet more tidying.

--- examples/try_visualise_trajectories.py
+++ examples/try_visualise_trajectories.py
@@ -8,6 +8,9 @@ from smg.utility.trajectory_util import TrajectoryUtil
 
 
 def main():
+    # TODO: I ultimately want to make this visualiser a lot more general and put it somewhere more central.
+
+    # Load in the trajectories saved by the monocular pose globaliser example.
     relocaliser_trajectory: List[Tuple[float, np.ndarray]] = TrajectoryUtil.load_tum_trajectory(
         "trajectory-relocaliser.txt"
     )
@@ -18,10 +21,12 @@ def main():
         "trajectory-tracker-unscaled.txt"
     )
 
+    # Smooth the trajectories using Laplacian smoothing to make the visualisation look a bit nicer.
     relocaliser_trajectory = TrajectoryUtil.smooth_trajectory(relocaliser_trajectory)
     tracker_trajectory = TrajectoryUtil.smooth_trajectory(tracker_trajectory)
     unscaled_tracker_trajectory = TrajectoryUtil.smooth_trajectory(unscaled_tracker_trajectory)
 
+    # Create the Open3D geometries for the visualisation.
     grid: LineSet = VisualisationUtil.make_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1])
     relocaliser_geoms: List[Geometry] = VisualisationUtil.make_geometries_for_trajectory(
         relocaliser_trajectory, (0.0, 1.0, 0.0)
@@ -32,6 +37,8 @@ def main():
     unscaled_tracker_geoms: List[Geometry] = VisualisationUtil.make_geometries_for_trajectory(
         unscaled_tracker_trajectory, (1.0, 0.0, 0.0)
     )
+
+    # Visualise the geometries.
     VisualisationUtil.visualise_geometries(
         [grid] + relocaliser_geoms + tracker_geoms + unscaled_tracker_geoms
     )

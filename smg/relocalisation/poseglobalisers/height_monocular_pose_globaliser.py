f6ff157b8582c8b1d6ac5de23eeed368bfba7ade
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-08-03 13:33:39 +0100

    Start tidying up a bit.

--- smg/relocalisation/poseglobalisers/height_monocular_pose_globaliser.py
+++ smg/relocalisation/poseglobalisers/height_monocular_pose_globaliser.py
@@ -5,13 +5,19 @@ from typing import Optional
 
 
 class HeightMonocularPoseGlobaliser:
-    """TODO"""
+    """
+    Used to correct the scale of monocular poses by using the known (e.g. sensed) metric height of the camera.
+
+    .. note::
+        This is particularly useful for globalising the camera poses of drones with an accurate height sensor,
+        such as the DJI Tello.
+    """
 
     # CONSTRUCTOR
 
     def __init__(self, *, debug: bool = False):
         """
-        TODO
+        Construct a height-based monocular pose globaliser.
 
         :param debug:   Whether or not to output debug messages.
         """
@@ -19,6 +25,7 @@ class HeightMonocularPoseGlobaliser:
         self.__height_movement_sum: float = 0.0
         self.__last_height: Optional[float] = None
         self.__last_tracker_pos: Optional[np.ndarray] = None
+        self.__metric_w_t_i: Optional[np.ndarray] = None
         self.__reference_height: Optional[float] = None
         self.__scale: float = 1.0
         self.__tracker_i_t_r: Optional[np.ndarray] = None
@@ -27,107 +34,94 @@ class HeightMonocularPoseGlobaliser:
         self.__up_count: int = 0
         self.__up_sum: np.ndarray = np.zeros(3)
 
-    # PROPERTIES
-
-    @property
-    def up(self) -> Optional[np.ndarray]:
-        return self.__up
-
     # PUBLIC METHODS
 
-    def apply(self, tracker_i_t_c: np.ndarray, *, suppress_scaling: bool = False) -> np.ndarray:
-        # Determine the scale to use (this will be the scale we've estimated, unless we're suppressing scaling).
-        scale: float = self.__scale if not suppress_scaling else 1.0
+    def apply(self, tracker_i_t_c: np.ndarray) -> np.ndarray:
+        """
+        Convert a non-metric transformation from current camera space to initial camera space into a metric
+        transformation from current camera space to world space.
 
-        # Make a metric transformation from reference space to initial camera space.
-        # TODO: This could be calculated in advance.
-        metric_tracker_i_t_r: np.ndarray = self.__tracker_i_t_r.copy()
-        metric_tracker_i_t_r[0:3, 3] *= scale
+        .. note::
+            "World" space is a coordinate system whose origin is on the ground below the reference origin,
+            and whose x and z axes are level with the ground (the y axis points downwards).
+        .. note::
+            The transformations output by the monocular tracker are assumed to be non-metric, and their scale
+            may drift over time.
 
-        # Make a metric transformation from initial camera space to world space.
-        # TODO: This could also be calculated in advance.
-        # wTi = wTr . (iTr)^-1
-        # w_t_r: np.ndarray = np.eye(4)
-        # w_t_r[0:3, 3] = np.array([0, self.__reference_height, 0])
-        # metric_tracker_w_t_i: np.ndarray = w_t_r @ np.linalg.inv(metric_tracker_i_t_r)
-        metric_tracker_w_t_i: np.ndarray = np.linalg.inv(metric_tracker_i_t_r)
-        metric_tracker_w_t_i[0:3, 3] += self.__reference_height * self.__up
-
-        w_t_r = metric_tracker_w_t_i @ metric_tracker_i_t_r
-        print("wTr:")
-        print(w_t_r)
-
-        print(f"Up Angle: {vg.angle(self.__up, np.array([0, -1, 0]))}")
-        angle: float = vg.angle(self.__up, np.array([0, -1, 0]), units="rad")
-        w_t_r: np.ndarray = np.eye(4)
-        cos_angle, sin_angle = np.cos(angle), np.sin(angle)
-        w_t_r[1, 1] = w_t_r[2, 2] = cos_angle
-        w_t_r[1, 2] = sin_angle
-        w_t_r[2, 1] = -sin_angle
-        w_t_r[1, 3] = -self.__reference_height
-        print(w_t_r)
-        metric_tracker_w_t_i = w_t_r @ np.linalg.inv(metric_tracker_i_t_r)
-
-        # from smg.rigging.cameras import SimpleCamera
-        # from smg.rigging.helpers import CameraPoseConverter
-        # look = vg.normalize(np.cross(self.__up, np.array([1, 0, 0])))
-        # print(f"Look: {look}")
-        # world_cam: SimpleCamera = SimpleCamera(-self.__reference_height * self.__up, look, self.__up)
-        # new_w_t_r: np.ndarray = CameraPoseConverter.camera_to_pose(world_cam)
-        # print(new_w_t_r)
-        # metric_tracker_w_t_i = new_w_t_r @ np.linalg.inv(metric_tracker_i_t_r)
-        #
-        # reference_cam: SimpleCamera = CameraPoseConverter.pose_to_camera(np.linalg.inv(metric_tracker_i_t_r))
-
+        :param tracker_i_t_c:   A non-metric transformation from current camera space to initial camera space,
+                                as estimated by the tracker.
+        :return:                A metric transformation from current camera space to world space.
+        """
         # Make a metric transformation from current camera space to initial camera space.
-        metric_tracker_i_t_c: np.ndarray = tracker_i_t_c.copy()
-        metric_tracker_i_t_c[0:3, 3] *= scale
+        metric_i_t_c: np.ndarray = tracker_i_t_c.copy()
+        metric_i_t_c[0:3, 3] *= self.__scale
 
-        # Make a metric transformation from current camera space to world space.
+        # Make and return a metric transformation from current camera space to world space.
         # wTc = wTi . iTc
-        tracker_w_t_c: np.ndarray = metric_tracker_w_t_i @ metric_tracker_i_t_c
-
-        return tracker_w_t_c
-
-    def reset(self) -> None:
-        self.__height_movement_sum = 0.0
-        self.__last_height = None
-        self.__last_tracker_pos = None
-        self.__reference_height = None
-        self.__scale = 1.0
-        self.__tracker_i_t_r = None
-        self.__tracker_movement_sum = 0.0
-        self.__up = None
-        self.__up_count = 0
-        self.__up_sum = np.zeros(3)
+        return self.__metric_w_t_i @ metric_i_t_c
 
     def train(self, tracker_i_t_c: np.ndarray, height: float) -> None:
+        """
+        Train the globaliser using a sample consisting of a non-metric pose estimated by the tracker
+        and the known (e.g. sensed) metric height of the camera at the same point in time.
+
+        :param tracker_i_t_c:   A non-metric transformation from current camera space to initial camera space,
+                                as estimated by the tracker.
+        :param height:          The known (e.g. sensed) metric height of the camera.
+        """
+        # Get the non-metric camera position estimated by the tracker.
         tracker_pos: np.ndarray = tracker_i_t_c[0:3, 3]
 
+        # If this is the first sample we've received:
         if self.__last_height is None:
+            # Use the current non-metric tracker pose and metric height to define the reference space.
             self.__reference_height = height
             self.__tracker_i_t_r = tracker_i_t_c.copy()
+
+            # Set the height and tracker position of the most recent sample.
             self.__last_height = height
             self.__last_tracker_pos = tracker_pos
+
+        # Otherwise:
         else:
+            # Compute the absolute change in metric height since the last sample.
             height_movement: float = abs(height - self.__last_height)
+
+            # If the height has changed by at least 2cm:
             if height_movement >= 0.02:
+                # Compute the non-metric camera movement estimated by the tracker since the last sample.
                 tracker_offset: np.ndarray = tracker_pos - self.__last_tracker_pos
                 tracker_movement: float = np.linalg.norm(tracker_offset)
 
+                # Update the two records of how far we've moved overall during training. Note that the height one
+                # will be metric, whilst the tracker one will be non-metric.
                 self.__height_movement_sum += height_movement
                 self.__tracker_movement_sum += tracker_movement
 
+                # As long as we've moved at least a bit (note that 0.1 is a non-metric measurement, so shouldn't be
+                # interpreted to mean 10cm):
                 if self.__tracker_movement_sum > 0.1:
+                    # Estimate the scale.
                     self.__scale = self.__height_movement_sum / self.__tracker_movement_sum
 
+                    # Compute an estimate of the up vector from the camera movement since the last sample,
+                    # as estimated by the tracker. Note that we assume the camera only moves either up or
+                    # down during training. The initial up estimate should point roughly in the direction
+                    # of either -y (up) or y (down). We multiply it by -1 if needed to make it point in
+                    # the direction of -y.
                     up_estimate: np.ndarray = tracker_offset.copy()
                     if up_estimate[1] > 0:
                         up_estimate *= -1
+
+                    # Use the estimate to update the overall up vector (this is just an average of all our estimates).
                     self.__up_sum += up_estimate
                     self.__up_count += 1
                     self.__up = vg.normalize(self.__up_sum / self.__up_count)
 
+                    # Update the metric transformation from initial camera space to world space.
+                    self.__update_w_t_i()
+
+                    # If we're debugging, print out some relevant values.
                     if self.__debug:
                         print(
                             height_movement, tracker_movement, tracker_offset,
@@ -135,5 +129,27 @@ class HeightMonocularPoseGlobaliser:
                             self.__scale, self.__up
                         )
 
+                # Update the height and tracker position of the most recent sample.
                 self.__last_height = height
                 self.__last_tracker_pos = tracker_pos
+
+    # PRIVATE METHODS
+
+    def __update_w_t_i(self) -> None:
+        """Update the metric transformation from initial camera space to world space."""
+        # Make a metric transformation from reference space to world space.
+        metric_w_t_r: np.ndarray = np.eye(4)
+        angle: float = vg.angle(self.__up, np.array([0, -1, 0]), units="rad")
+        cos_angle, sin_angle = np.cos(angle), np.sin(angle)
+        metric_w_t_r[1, 1] = metric_w_t_r[2, 2] = cos_angle
+        metric_w_t_r[1, 2] = sin_angle
+        metric_w_t_r[2, 1] = -sin_angle
+        metric_w_t_r[1, 3] = -self.__reference_height
+
+        # Make a metric transformation from reference space to initial camera space.
+        metric_i_t_r: np.ndarray = self.__tracker_i_t_r.copy()
+        metric_i_t_r[0:3, 3] *= self.__scale
+
+        # Update the metric transformation from initial camera space to world space.
+        # wTi = wTr . (iTr)^-1
+        self.__metric_w_t_i = metric_w_t_r @ np.linalg.inv(metric_i_t_r)

0421526578567df3fc43e6e743241c7e8ab7b677
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-08-03 10:03:21 +0100

    Get the world coordinate system in the right place.

--- smg/relocalisation/poseglobalisers/height_monocular_pose_globaliser.py
+++ smg/relocalisation/poseglobalisers/height_monocular_pose_globaliser.py
@@ -27,10 +27,15 @@ class HeightMonocularPoseGlobaliser:
         self.__up_count: int = 0
         self.__up_sum: np.ndarray = np.zeros(3)
 
+    # PROPERTIES
+
+    @property
+    def up(self) -> Optional[np.ndarray]:
+        return self.__up
+
     # PUBLIC METHODS
 
-    def apply(self, tracker_i_t_c: np.ndarray, height: Optional[float], *,
-              suppress_scaling: bool = False) -> np.ndarray:
+    def apply(self, tracker_i_t_c: np.ndarray, *, suppress_scaling: bool = False) -> np.ndarray:
         # Determine the scale to use (this will be the scale we've estimated, unless we're suppressing scaling).
         scale: float = self.__scale if not suppress_scaling else 1.0
 
@@ -48,6 +53,32 @@ class HeightMonocularPoseGlobaliser:
         metric_tracker_w_t_i: np.ndarray = np.linalg.inv(metric_tracker_i_t_r)
         metric_tracker_w_t_i[0:3, 3] += self.__reference_height * self.__up
 
+        w_t_r = metric_tracker_w_t_i @ metric_tracker_i_t_r
+        print("wTr:")
+        print(w_t_r)
+
+        print(f"Up Angle: {vg.angle(self.__up, np.array([0, -1, 0]))}")
+        angle: float = vg.angle(self.__up, np.array([0, -1, 0]), units="rad")
+        w_t_r: np.ndarray = np.eye(4)
+        cos_angle, sin_angle = np.cos(angle), np.sin(angle)
+        w_t_r[1, 1] = w_t_r[2, 2] = cos_angle
+        w_t_r[1, 2] = sin_angle
+        w_t_r[2, 1] = -sin_angle
+        w_t_r[1, 3] = -self.__reference_height
+        print(w_t_r)
+        metric_tracker_w_t_i = w_t_r @ np.linalg.inv(metric_tracker_i_t_r)
+
+        # from smg.rigging.cameras import SimpleCamera
+        # from smg.rigging.helpers import CameraPoseConverter
+        # look = vg.normalize(np.cross(self.__up, np.array([1, 0, 0])))
+        # print(f"Look: {look}")
+        # world_cam: SimpleCamera = SimpleCamera(-self.__reference_height * self.__up, look, self.__up)
+        # new_w_t_r: np.ndarray = CameraPoseConverter.camera_to_pose(world_cam)
+        # print(new_w_t_r)
+        # metric_tracker_w_t_i = new_w_t_r @ np.linalg.inv(metric_tracker_i_t_r)
+        #
+        # reference_cam: SimpleCamera = CameraPoseConverter.pose_to_camera(np.linalg.inv(metric_tracker_i_t_r))
+
         # Make a metric transformation from current camera space to initial camera space.
         metric_tracker_i_t_c: np.ndarray = tracker_i_t_c.copy()
         metric_tracker_i_t_c[0:3, 3] *= scale
@@ -55,8 +86,6 @@ class HeightMonocularPoseGlobaliser:
         # Make a metric transformation from current camera space to world space.
         # wTc = wTi . iTc
         tracker_w_t_c: np.ndarray = metric_tracker_w_t_i @ metric_tracker_i_t_c
-
-        # TODO: Height fixing
 
         return tracker_w_t_c
 
